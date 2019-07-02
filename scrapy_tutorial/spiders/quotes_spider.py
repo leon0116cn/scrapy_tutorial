6c1f414d681e0c3d84d68c37d2d9c8770be91c8e
Leon <leon0116cn@sina.com>
2019-07-02 13:50:57 +0800

    创建了Quotes_Spider爬虫

--- scrapy_tutorial/spiders/quotes_spider.py
+++ scrapy_tutorial/spiders/quotes_spider.py
@@ -3,10 +3,7 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
-    ]
+    start_urls = ['http://quotes.toscrape.com/']
 
     # def start_requests(self):
     #     urls = [
@@ -17,8 +14,40 @@ class QuotesSpider(scrapy.Spider):
     #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'Quotes-{}.html'.format(page)
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file {}'.format(filename))
+        for quote in response.css('div.quote'):
+            yield {
+                'title': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall()
+            }
+
+        # next_page = response.css("li.next a::attr(href)").get()
+        # if next_page is not None:
+            # next_page = response.urljoin(next_page)
+            # yield scrapy.Request(url=next_page, callback=self.parse)
+
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
+
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+    start_urls = start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        for a in response.css('.author + a'):
+            yield response.follow(a, self.parse_author)
+
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
+
+    def parse_author(self, response):
+        def extrace_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extrace_with_css('h3.author-title::text'),
+            'birthdate': extrace_with_css('.author-born-date::text'),
+            'bio': extrace_with_css('.author-born-location::text'),
+            'description': extrace_with_css('.author-description::text')
+        }
